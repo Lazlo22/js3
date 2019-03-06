68aa8f59e8dfca34ae0629a44f6f9db710d0691a
Lazlo22 <47634781+Lazlo22@users.noreply.github.com>
2019-03-06 17:19:59 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//First Task
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+//Second Task
+function circleCircumference(circle) {
+  return Math.PI*circle.radius*2; ;
+}
+
+//Third Task
